b3090c1f65efaadaf1f0b76f9deb290c21b09936
manducku <alwaysfun2183@gmail.com>
2016-03-29 00:11:35 +0900

    update Tag model: add Unique config to Tag

--- blogram/tags/models/tag.py
+++ blogram/tags/models/tag.py
@@ -5,6 +5,7 @@ class Tag(models.Model):
 
     name = models.CharField(
             max_length=20,
+            unique=True,
             )
 
     create_at = models.DateTimeField(auto_now_add=True,)
