edd50f18495eb79944ae60e3252ac708262520d1
manducku <alwaysfun2183@gmail.com>
2016-03-29 02:03:27 +0900

    update url.py: add login url

--- blogram/blogram/urls.py
+++ blogram/blogram/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from users.views import *
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', LoginView.as_view(), name="login"),
 ]
