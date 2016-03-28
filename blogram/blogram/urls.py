dd956b06ed0dd5a469b0117478273ed4d37560fd
manducku <alwaysfun2183@gmail.com>
2016-03-29 02:09:50 +0900

    make home html, view, url

--- blogram/blogram/urls.py
+++ blogram/blogram/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from users.views import *
+from blogram.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', LoginView.as_view(), name="login"),
+    url(r'^$', HomeView.as_view(), name="home"),
 ]
