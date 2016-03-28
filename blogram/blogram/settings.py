91475a866ee2d219338317b1c5f6862c6a58c7d6
manducku <alwaysfun2183@gmail.com>
2016-03-28 15:07:45 +0900

    update login HTML template

--- blogram/blogram/settings.py
+++ blogram/blogram/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogram',
     'users',
     'django_extensions',
 ]
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# custom Authentication settings
+AUTH_USER_MODEL = "users.User"
