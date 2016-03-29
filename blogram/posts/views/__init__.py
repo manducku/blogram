9c6e8cd9973985b321b8360cf08491333d8572b3
manducku <alwaysfun2183@gmail.com>
2016-03-29 22:26:23 +0900

    add CommentCreateView

--- blogram/posts/views/__init__.py
+++ blogram/posts/views/__init__.py
@@ -1,3 +1,4 @@
 from .list import PostListView
 from .detail import PostDetailView
 from .base import PostBaseView
+from .comment import CommentCreateView
---
+++ blogram/posts/views/comment.py
@@ -0,0 +1,24 @@
+from django.views.generic import CreateView
+
+from posts.models import Comment, Post
+
+class CommentCreateView(CreateView):
+
+    model = Comment
+    fields = ['content']
+
+    def form_valid(self, form):
+        post = Post.objects.get(
+                hash_id=self.kwargs.get('slug')
+                )
+
+        form.instance.post = post
+        form.instance.user = self.request.user
+
+        return super(CommentCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        post = Post.objects.get(
+                hash_id=self.kwargs.get('slug')
+                )
+        return post.get_absolute_url()
