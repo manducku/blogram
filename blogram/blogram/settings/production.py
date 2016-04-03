25c58a103eb3c2f1ced940a13f761041f918b746
manducku <alwaysfun2183@gmail.com>
2016-04-03 22:04:23 +0900

    add S3 setting

--- blogram/blogram/settings/production.py
+++ blogram/blogram/settings/production.py
@@ -1,7 +1,15 @@
 from .partials import *
-
+import os
 
 DEBUG = False
 
 ALLOWED_HOSTS = [
+       "*",
         ]
+
+STATICFILES_STORAGE = 'blogram.storage.S3PipelineCachedStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
