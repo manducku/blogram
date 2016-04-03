b00ac6142cd384bef04cc53579cf6b121f7ed621
manducku <alwaysfun2183@gmail.com>
2016-04-03 22:29:01 +0900

    add Cloud Front setting

--- blogram/blogram/settings/production.py
+++ blogram/blogram/settings/production.py
@@ -13,3 +13,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+
+AWS_S3_CUSTOM_DOMAIN = 'd15irv28b5ifxq.cloudfront.net'
+AWS_S3_URL_PROTOCOL = 'https'
+STATIC_URL = 'https://d15irv28b5ifxq.cloudfront.net/'
